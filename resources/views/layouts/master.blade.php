f79dcac936dc8a1bea12bfa7304b6e4ec921809c
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2018-08-08 16:09:11 +0200

    This is how to add css file to the blade template. In master template we need to call the href with asset(), it will automatically point to public folder. Then we point it to the css file. After that in layouts folder we need to create the layout for each part with the div-Tag. Then call it in the master.blade.php. The css file can be edited in the file in public folder

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Khoa Pham </title>
+	<link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
 </head>
 <body>
-	<h1>Khoa Pham</h1>
+	@include('layouts.header')
+	<div id="content">
+		<h1>Khoa Pham</h1>
 	
-	@yield('NoiDung')
+		@yield('NoiDung')
+	</div>
+	@include('layouts.footer')
 </body>
 </html>
