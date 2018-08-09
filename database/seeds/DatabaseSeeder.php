f0d7a96a33f2ab7926eea4b3f23336dc66d5ef33
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2018-08-09 17:15:37 +0200

    This is the practice with the Seeder. The seeder how we can add the data in the database. We can simply call it in run to execute. Or create the whole class to insert many record at once

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(userSeeder::class);
+
     }
 }
+
+class userSeeder extends Seeder
+{
+	public function run()
+	{
+		DB::table('users')->insert([
+			['name'	=> 'Atonias','email'=> str_random(3).'@gmail.com','password'=> bcrypt('matkhau')],
+			['name'	=> 'Mika','email'	=> str_random(3).'@gmail.com','password'=> bcrypt('matkhau')],
+			['name'	=> 'Chester','email'=> str_random(3).'@gmail.com','password'=> bcrypt('matkhau')]
+		]);
+	}
+}
+
+
