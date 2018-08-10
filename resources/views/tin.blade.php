39f36ad21f35d5bd80873ce94c909fb67714f4b2
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2018-08-10 13:30:15 +0200

    Some more function to use in pagination

--- resources/views/tin.blade.php
+++ resources/views/tin.blade.php
@@ -13,4 +13,6 @@
 	
 @endforeach
 
-{!! $tin->links() !!}
+{{--This line to create on the route a thing line abc=123. That is meanless but the other still work fine--}}
+{!! $tin->appends(['abc'=>'123'])->links() !!}
+
