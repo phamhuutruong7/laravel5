7033f2b35998ddf7ba4e1635080579c310918e18
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2018-08-10 11:33:28 +0200

    This is the login success

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,13 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests;
 
 class AuthController extends Controller
 {
-    //
+    //This example is working with table USERS 
+    //When we create the DB with the Seed, 
+    //that make Auth can automatically recognize this point to User table
+    //This require password to be hash
+    //If you inser directly a record with a not hash password, it will not work.
     public function login(Request $request)
     {
-    	echo $request['username']."<br>";
-    	echo $request['password'];
+    	$username = $request['username'];
+    	$password = $request['password'];
+    	if(Auth::attempt(['name'=>$username, 'password'=>$password]))
+    	{	//This 'name' and 'password' here is from table Users
+    		//If login succesful then attempt return true
+    		return view('thanhcong');
+    	}
+    	else
+    	{
+    		return view('dangnhap',['error'=>'Dang nhap that bai']);
+    	}
     }
 }
--- resources/views/dangnhap.blade.php
+++ resources/views/dangnhap.blade.php
@@ -1,3 +1,5 @@
+{{ $error or ''}}
+{{--If error is exist, it will print out the $error. If not it will print the string in the ''--}}
 <form action="{{route('login')}}" method="post">
 	{{csrf_field()}}
 	<input type="text" name="username" placeholder="username">
